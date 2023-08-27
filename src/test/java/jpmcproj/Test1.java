0dba124b28f4f54563f0dfce258c8ff568a67f3f
RAM <RAM@LAPTOP-M9FVOIFN>
2023-08-27 23:06:47 +0530

    coomit 1 msg

---
+++ src/test/java/jpmcproj/Test1.java
@@ -0,0 +1,24 @@
+package jpmcproj;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("hi rk");
+		// TODO Auto-generated method stub
+		
+		//WebDriverManager.chromedriver().setup();
+		WebDriverManager.edgedriver().setup();
+		EdgeDriver driver = new EdgeDriver();
+	//	ChromeDriver driver = new ChromeDriver();
+		driver.get("https://gmail.com");
+		
+
+	}
+
+}
---
+++ src/test/java/jpmcproj/Test3.java
@@ -0,0 +1,27 @@
+package jpmcproj;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Test3 {
+
+	//public static void main(String[] args) {
+	@Test
+	public void method1()
+	{
+		System.out.println("hi rk");
+		// TODO Auto-generated method stub
+		
+		//WebDriverManager.chromedriver().setup();
+		WebDriverManager.edgedriver().setup();
+		EdgeDriver driver = new EdgeDriver();
+	//	ChromeDriver driver = new ChromeDriver();
+		driver.get("https://facebook.com");
+		
+
+	}
+
+}
